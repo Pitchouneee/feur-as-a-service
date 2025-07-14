bafa7338e5b8e52845652f5e9bbedeb0776c4271
Corentin Bringer <corentinbringer@ik.me>
2025-07-14 09:41:51 +0200

    name bot

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const client = new Client({
 });
 
 client.on('ready', () => {
-    console.log(`QuoiFeurBot connecté`);
+    console.log(`FeurBot connecté`);
 });
 
 client.on('messageCreate', async (message) => {
